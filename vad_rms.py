2f2bb2b51b22f42d5b3a067c28f8d384403154c2
jbraga <juanbragabrum@gmail.com>
2016-03-17 15:30:06 -0300

    comb filter for harmonicity added

--- vad_rms.py
+++ vad_rms.py
@@ -110,3 +110,29 @@ plt.legend(loc='best')
 plt.tight_layout()
 
 plt.show()
+
+#%%
+
+from scipy import signal
+import math
+
+alpha = 1
+b = np.array([1, 0, 0, alpha])
+w, h = signal.freqz(b)
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+plt.title('Digital filter frequency response')
+ax1 = fig.add_subplot(111)
+
+plt.plot(w*(22050/(2*math.pi)), 20 * np.log10(abs(h)), 'b')
+plt.ylabel('Amplitude [dB]', color='b')
+plt.xlabel('Frequency [rad/sample]')
+
+ax2 = ax1.twinx()
+angles = np.unwrap(np.angle(h))
+plt.plot(w, angles, 'g')
+plt.ylabel('Angle (radians)', color='g')
+plt.grid()
+plt.axis('tight')
+plt.show()
