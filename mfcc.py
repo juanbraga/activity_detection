4b5776ad5382ae82de889c7e786f3549f4ad8188
jbraga@ThinkPad <juanbragabrum@gmail.com>
2016-04-11 19:59:12 -0300

    svm classifier for short time features

--- mfcc.py
+++ mfcc.py
@@ -107,3 +107,20 @@ if __name__ == "__main__":
     ax.w_zaxis.set_ticklabels([])
     
     plt.show()
+    
+    #%%
+    fig = plt.figure(4, figsize=(8, 6))
+    ax = Axes3D(fig, elev=-150, azim=110)
+    X_pca = decomposition.TruncatedSVD(n_components=3).fit_transform(total_mfcc[:-1,:])
+    ax.scatter( X_pca[:, 0],  X_pca[:, 1],  X_pca[:, 2], c=labels[:-1],
+           cmap=plt.cm.Paired)
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("1st eigenvector")
+    ax.w_xaxis.set_ticklabels([])
+    ax.set_ylabel("2nd eigenvector")
+    ax.w_yaxis.set_ticklabels([])
+    ax.set_zlabel("3rd eigenvector")
+    ax.w_zaxis.set_ticklabels([])
+    
+    plt.show()
+    
