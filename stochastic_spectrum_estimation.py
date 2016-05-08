dcee0899f8952091efa80b0944799fadc476063a
jbraga@dell <juanbragabrum@gmail.com>
2016-05-08 01:59:03 -0300

    .mat for signal reconstruction on matlab generated

--- stochastic_spectrum_estimation.py
+++ stochastic_spectrum_estimation.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
+import scipy.io 
 
 def mean_filter(M, n=20):
     
@@ -45,14 +46,15 @@ if __name__ == "__main__":
     for row in cr:
         dataset.append(row[0]) 
     
-    audio_file = dataset[4] + '_mono.wav'
+    audio_file = dataset[6] + '_mono.wav'
+    print audio_file
     fs, audio = wav.read(audio_file)
     t = np.arange(len(audio)) * float(1)/fs
         
-    plt.figure()
-    plt.plot(t,audio)
-    plt.grid()
-    plt.axis('tight')
+#    plt.figure()
+#    plt.plot(t,audio)
+#    plt.grid()
+#    plt.axis('tight')
 
     nfft = 512
     noverlap=0#3*(nfft/4)
@@ -61,43 +63,43 @@ if __name__ == "__main__":
                                      noverlap=noverlap, nfft=None, detrend='constant', 
                                      return_onesided=True, scaling='density', axis=-1)
     
-    plt.figure()
-    plt.pcolormesh(t_S, f, 20*np.log(Sxx))
-    plt.axis('tight')
-    plt.show()
+#    plt.figure()
+#    plt.pcolormesh(t_S, f, 20*np.log(Sxx))
+#    plt.axis('tight')
+#    plt.show()
     
     Sxx_filt = mean_filter(Sxx, n=3)
     
-    plt.figure()
-    plt.pcolormesh(t_S, f, 20*np.log(Sxx_filt))
-    plt.axis('tight')
-    plt.show()
+#    plt.figure()
+#    plt.pcolormesh(t_S, f, 20*np.log(Sxx_filt))
+#    plt.axis('tight')
+#    plt.show()
     
 #%%    
     Sxx_filt_inv = reciprocal(Sxx_filt)
     
-    plt.figure()
-    plt.pcolormesh(t_S, f, 20*np.log(Sxx_filt_inv))
-    plt.axis('tight')
-    plt.show()
+#    plt.figure()
+#    plt.pcolormesh(t_S, f, 20*np.log(Sxx_filt_inv))
+#    plt.axis('tight')
+#    plt.show()
     
 #%%
    
     Sxx_filt_inv_filt = mean_filter(Sxx_filt_inv, n=25)
     
-    plt.figure()
-    plt.pcolormesh(t_S, f, 20*np.log(Sxx_filt_inv_filt))
-    plt.axis('tight')
-    plt.show()
+#    plt.figure()
+#    plt.pcolormesh(t_S, f, 20*np.log(Sxx_filt_inv_filt))
+#    plt.axis('tight')
+#    plt.show()
     
 #%%
    
     SSE = reciprocal(Sxx_filt_inv_filt)
     
-    plt.figure()
-    plt.pcolormesh(t_S, f, 20*np.log(SSE))
-    plt.axis('tight')
-    plt.show()
+#    plt.figure()
+#    plt.pcolormesh(t_S, f, 20*np.log(SSE))
+#    plt.axis('tight')
+#    plt.show()
     
 ##%%
 #    plt.figure(figsize=(18,6))
@@ -106,64 +108,67 @@ if __name__ == "__main__":
 
 #%%
     env = time_env(SSE)    
-    plt.figure(figsize=(18,6))  
-    plt.plot(t,audio)    
-    plt.plot(t_S, env*max(audio), color='red')
-    plt.grid()
+#    plt.figure(figsize=(18,6))  
+#    plt.plot(t,audio)    
+#    plt.plot(t_S, env*max(audio), color='red')
+#    plt.grid()
 
 #%%
-    plt.figure()
-    plt.subplot(4,1,(1,3))
-    plt.pcolormesh(t_S[(2<t_S) & (t_S<8)], f, 20*np.log(Sxx[:,(2<t_S) & (t_S<8)]))
-    plt.axis('tight')
-    plt.subplot(4,1,4)
-    plt.plot(t_S[(2<t_S) & (t_S<8)], env[(2<t_S) & (t_S<8)], color='red')
-    plt.axis('tight')
-    plt.grid()
-    plt.show()    
+#    plt.figure()
+#    plt.subplot(4,1,(1,3))
+#    plt.pcolormesh(t_S[(2<t_S) & (t_S<8)], f, 20*np.log(Sxx[:,(2<t_S) & (t_S<8)]))
+#    plt.axis('tight')
+#    plt.subplot(4,1,4)
+#    plt.plot(t_S[(2<t_S) & (t_S<8)], env[(2<t_S) & (t_S<8)], color='red')
+#    plt.axis('tight')
+#    plt.grid()
+#    plt.show()    
 
 
 #%%
-    plt.figure()
-    plt.subplot(4,1,(1,3))
-    plt.pcolormesh(t_S, f, 20*np.log(Sxx))
-    plt.axis('tight')
-    plt.subplot(4,1,4)
-    plt.plot(t_S, env, color='red')
-    plt.axis('tight')
-    plt.grid()
-    plt.show()
+#    plt.figure()
+#    plt.subplot(4,1,(1,3))
+#    plt.pcolormesh(t_S, f, 20*np.log(Sxx))
+#    plt.axis('tight')
+#    plt.subplot(4,1,4)
+#    plt.plot(t_S, env, color='red')
+#    plt.axis('tight')
+#    plt.grid()
+#    plt.show()
 
 #%%    
 
-    plt.figure(figsize=(18,6))  
-    plt.plot(t_S, env, color='red')
-    plt.grid()
+#    plt.figure(figsize=(18,6))  
+#    plt.plot(t_S, env, color='red')
+#    plt.grid()
     
 
 #%%    
     env_filt = mean_filter(env, n=4)
-    plt.figure(figsize=(18,6))  
-    plt.plot(t_S, env_filt, color='red')
-    plt.grid()
+#    plt.figure(figsize=(18,6))  
+#    plt.plot(t_S, env_filt, color='red')
+#    plt.grid()
     
 #%%
-    plt.figure()
-    plt.subplot(4,1,1)
-    plt.pcolormesh(t_S, f[f<10000], 20*np.log(Sxx[f<10000,:]))
-    plt.axis('tight')
-    plt.subplot(4,1,(2,4))
-    plt.plot(t_S, env_filt, color='red')
-    plt.axis('tight')
-    plt.grid()
-    plt.show()
+#    plt.figure()
+#    plt.subplot(4,1,1)
+#    plt.pcolormesh(t_S, f[f<10000], 20*np.log(Sxx[f<10000,:]))
+#    plt.axis('tight')
+#    plt.subplot(4,1,(2,4))
+#    plt.plot(t_S, env_filt, color='red')
+#    plt.axis('tight')
+#    plt.grid()
+#    plt.show()
     
     #%%
     np.savetxt("sse_env.csv", np.c_[t_S, env], delimiter=",")
 
     #%%
 
-    plt.figure()
-    plt.pcolormesh(t_S, f, 20*np.log(Sxx-SSE))
-    plt.axis('tight')
-    plt.show()    
+#    plt.figure()
+#    plt.pcolormesh(t_S, f, 20*np.log(Sxx-SSE))
+#    plt.axis('tight')
+#    plt.show()    
+    
+    #%%
+    scipy.io.savemat('sse', mdict={'sse':SSE,'t':t_S,'f':f,'win':np.hamming(nfft),'noverlap':noverlap})
