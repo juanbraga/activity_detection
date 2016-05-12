3e0aba8bead63417bde549b474f933d20b620671
jbraga@ThinkPad <juanbragabrum@gmail.com>
2016-05-12 19:29:24 -0300

    resynth first steps

--- analyze_synthesize.py
+++ analyze_synthesize.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     for row in cr:
         dataset.append(row[0]) 
     
-    audio_file = dataset[3] + '_mono.wav'
+    audio_file = dataset[6] + '_mono.wav'
     print audio_file
     fs, audio = wav.read(audio_file)
     t = np.arange(len(audio)) * float(1)/fs
@@ -47,15 +47,34 @@ if __name__ == "__main__":
     plt.pcolormesh(t_S, f, 20*np.log(Sxx))
     plt.axis('tight')
     
-    X = fft.fft(audio)
-    X_r = fft.rfft(audio)
-    
-    #%%
+    X = np.fft.fft(audio)
+    freq = np.fft.fftfreq(X.size, 1/float(fs))
+        
+    resynth = np.fft.ifft(X);
+
+#%%
+
     plt.figure(figsize=(18,6))
-    plt.plot(abs(X_r),label='rfft')
-    plt.plot(abs(X),label='fft')
-    plt.legend(loc='best')
+    plt.subplot(3,1,1)
+    plt.plot(t, audio)
+    plt.grid()
+    plt.axis('tight')    
+    plt.subplot(3,1,2)
+    plt.plot(freq, abs(X),label='fft')
+    plt.grid()
+    plt.axis('tight')    
+    plt.subplot(3,1,3)
+    plt.plot(t, resynth.real,label='fft')
+    plt.grid()
+    plt.axis('tight')    
+                              
+    diff = audio - resynth.real
+    
+    plt.figure()
+    plt.plot(t, diff)
     plt.grid()
     plt.axis('tight')
-                                     
+    
+    wav.write("resynth.wav",fs,resynth.real)
+    wav.write("diff.wav",fs,diff)
     
