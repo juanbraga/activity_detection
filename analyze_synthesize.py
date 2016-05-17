576534d08b723b3ba3332579c5fe9f3f4c310a6e
jbraga@ThinkPad <juanbragabrum@gmail.com>
2016-05-17 20:47:39 -0300

    finally resynth of SSE done

--- analyze_synthesize.py
+++ analyze_synthesize.py
@@ -6,13 +6,47 @@ Created on Mon May  9 18:10:49 2016
 """
 
 import numpy as np
+import stochastic_spectrum_estimation as sse
 from scipy import signal
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
-import scipy.fftpack as fft
 
 def stft(x, fs, win, hop):
-    pass
+    
+    S = np.empty([(win),(len(x)/hop)+1],dtype='complex64')
+    
+    for i in range(0,(len(x)/hop)+1):    
+        frame=x[i*hop:i*hop+win]
+        frame = np.hamming(len(frame))*frame        
+        X = np.fft.fft(frame,nfft)
+        S[:,i] = X 
+        
+    freq = np.fft.fftfreq(win, 1/float(fs))
+    t = np.arange((len(x)/hop)+1) * float(hop)/fs   
+    
+    return S, freq, t
+    
+
+def istft(X,fs,win,hop,filename="resynth.wav"):
+    
+    resynth = np.empty([hop*(X.shape[1]-1)+win,], dtype='float64')
+    for i in range(0,X.shape[1]):    
+        iX = np.fft.ifft(X[:,i])
+        aux_resynth = np.zeros(hop*(X.shape[1]-1)+win, dtype='float64')
+        aux_resynth[i*hop:i*hop+win]=iX.real
+        resynth = resynth + aux_resynth 
+    
+    resynth_int16 = np.array(resynth,dtype='int16')
+    wav.write(filename, fs, resynth_int16)
+    t=np.arange(hop*X.shape[1]) * float(1)/fs
+    
+    return resynth, t 
+    
+def P2R(radii, angles):
+    return radii * np.exp(1j*angles)
+
+def R2P(x):
+    return np.abs(x), np.angle(x)
 
 if __name__ == "__main__":  
 
@@ -36,45 +70,47 @@ if __name__ == "__main__":
 
     nfft = 512
     noverlap=(3*nfft)/4
+    hop = nfft-noverlap
     
-    f, t_S, S = signal.spectrogram(audio, fs, window='hamming', nperseg=nfft, 
-                                     noverlap=noverlap, nfft=None, detrend='constant',
-                                     return_onesided=True, scaling='density', axis=-1, mode='complex')
-                                    
-    Sxx = np.abs(S) 
+#    f, t_S, S = signal.spectrogram(audio, fs, window='hamming', nperseg=nfft, 
+#                                     noverlap=noverlap, nfft=None, detrend='constant',
+#                                     return_onesided=False, scaling='density', axis=-1, mode='complex')
+#                                    
+#    Sxx = np.abs(S) 
+#    
+#    plt.figure()
+#    plt.subplot(2,1,1)  
+#    plt.pcolormesh(t_S, f, 20*np.log(np.abs(S)))
+#    plt.axis('tight')
+#    plt.subplot(2,1,2)
+#    plt.pcolormesh(t_S, f, np.angle(S))
+#    plt.axis('tight')            
+#
+#    spec, f_spec, t_spec = stft(audio, fs, nfft, (nfft-noverlap))
+#    resynth, t_resynth = istft(spec,fs,nfft,(nfft-noverlap))
+#    
+#    plt.figure()
+#    plt.subplot(2,1,1)
+#    plt.pcolormesh(t_spec, f_spec, 20*np.log(np.abs(spec)))
+#    plt.axis('tight')    
+#    plt.subplot(2,1,2) 
+#    plt.pcolormesh(t_spec, f_spec, np.angle(spec))
+#    plt.axis('tight')
+     
+    S, f, t_S = stft(audio, fs, nfft, (nfft-noverlap))
+    SSE, env = sse.sse(np.abs(S))
     
+    #%%
+     
     plt.figure()
-    plt.pcolormesh(t_S, f, 20*np.log(Sxx))
+    plt.subplot(2,1,1)
+    plt.pcolormesh(t_S, f, 20*np.log(np.abs(S)))
+    plt.axis('tight')    
+    plt.subplot(2,1,2) 
+    plt.pcolormesh(t_S, f, 20*np.log(SSE))
     plt.axis('tight')
-    
-    X = np.fft.fft(audio)
-    freq = np.fft.fftfreq(X.size, 1/float(fs))
-        
-    resynth = np.fft.ifft(X);
 
-#%%
-
-    plt.figure(figsize=(18,6))
-    plt.subplot(3,1,1)
-    plt.plot(t, audio)
-    plt.grid()
-    plt.axis('tight')    
-    plt.subplot(3,1,2)
-    plt.plot(freq, abs(X),label='fft')
-    plt.grid()
-    plt.axis('tight')    
-    plt.subplot(3,1,3)
-    plt.plot(t, resynth.real,label='fft')
-    plt.grid()
-    plt.axis('tight')    
-                              
-    diff = audio - resynth.real
+#%%    
     
-    plt.figure()
-    plt.plot(t, diff)
-    plt.grid()
-    plt.axis('tight')
-    
-    wav.write("resynth.wav",fs,resynth.real)
-    wav.write("diff.wav",fs,diff)
+    S_resynth = P2R(SSE,np.angle(S))
     
